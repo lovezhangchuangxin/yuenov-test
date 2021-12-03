5c03855fefb48e86362fd8b6a22630b38d49083c
zhangchuangxin <2911331070@qq.com>
2021-12-03 16:14:34 +0800

    feat: init

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './css/index.less'
+import './js/alert.js'
+
+function A() {
+    return (
+        <h1>你好啊 </h1>
+    )
+}
+
+ReactDOM.render(<A />, document.getElementById('root'));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            esModule: false
+                        }
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|gif|svg|jpe?g)$/,
+                type: 'asset',
+                generator: {
+                    filename: 'img/[name].[hash:6][ext]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 500 * 1024
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                    'ts-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'hexiechuangxin',
+            template: './public/index.html'
+        })
+    ]
+}
