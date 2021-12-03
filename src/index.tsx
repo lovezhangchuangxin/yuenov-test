07ec8741e596d83cdc9f18e84fad7a04215347b2
zhangchuangxin <2911331070@qq.com>
2021-12-03 16:44:18 +0800

    refactor: make the project clear

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './css/index.less'
-import './js/alert.js'
 
 function A() {
     return (
-        <h1>你好啊 </h1>
+        <h1>Hello World</h1>
     )
 }
 
